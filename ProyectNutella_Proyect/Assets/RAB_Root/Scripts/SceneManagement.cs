ba436c4e008301bf07757a1f65faa5d8ddee07b2
AdrianGalindoLlacer <adriangalindollacer@gmail.com>
2025-11-10 16:48:12 +0100

    Menu de pausa añadido

--- ProyectNutella_Proyect/Assets/RAB_Root/Scripts/SceneManagement.cs
+++ ProyectNutella_Proyect/Assets/RAB_Root/Scripts/SceneManagement.cs
@@ -3,6 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagement : MonoBehaviour
 {
+
+    public GameObject[] pauseUI; //index 0: button index 1: pausePanel
+
     public void LoadScene(int sceneToLoad)
     {
         SceneManager.LoadScene(sceneToLoad);
@@ -12,5 +15,19 @@ public class SceneManagement : MonoBehaviour
     {
         Debug.Log("Has cerrado el juego.");
         Application.Quit();
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        pauseUI[0].SetActive(false);
+        pauseUI[1].SetActive(true);
+    }
+
+    public void Unpause()
+    {
+        Time.timeScale = 1;
+        pauseUI[0].SetActive(true);
+        pauseUI[1].SetActive(false);
     }
 }
